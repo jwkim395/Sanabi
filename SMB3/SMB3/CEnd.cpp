90a18acdea1ceb9f62093e4c68ea4df512a35d17
kimjw395 <01kimjw@kookmin.ac.kr>
2023-11-18 22:30:44 +0900

    끝날때 course clear 글자 추가

--- SMB3/SMB3/CEnd.cpp
+++ SMB3/SMB3/CEnd.cpp
@@ -2,6 +2,8 @@
 #include "CEnd.h"
 #include "CAssetMgr.h"
 #include "CLevelMgr.h"
+#include "CLevel.h"
+#include "CBackGround.h"
 
 
 void CEnd::Overlap(CCollider* _OwnCol, CObj* _OtherObj, CCollider* _OtherCol)
@@ -14,6 +16,13 @@ void CEnd::BeginOverlap(CCollider* _OwnCol, CObj* _OtherObj, CCollider* _OtherCo
 		_OtherObj->GetMovement()->SetVelocity(Vec2(192.f, 0.f));
 		CLevelMgr::GetInst()->setmTime(4.f);
 		CLevelMgr::GetInst()->setMarioStatus(4);
+		CBackGround* CourseClear;
+		CourseClear = new CBackGround;
+		CourseClear->SetPos(Vec2(GetPos().x, GetPos().y - 300.f));
+		CourseClear->SetScale(Vec2(128.f, 16.f));
+		CourseClear->setImagePos(Vec2(0.f, 0.f), Vec2(128.f, 16.f));
+		CourseClear->setTexture(L"CourseClear", L"texture\\courseClear.png");
+		CLevelMgr::GetInst()->GetCurLevel()->GetLayer(BACKGROUND)->AddObject(CourseClear);
 	}
 }
 
